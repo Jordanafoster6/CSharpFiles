18cf56db9ddebb89adfebf8ce6f39423dbdaa939
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:42:58 -0600

    Reverse3

---
+++ Reverse3/IntegerFacts/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerFacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int high, low, sum, avg;
+          //  outIntegers(out high, out low, out sum, avg);
+            int[] nums = new int[20];
+            fillMethod(nums);
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+            
+            
+
+        }
+
+        private static void fillMethod(int[] nums)
+        {
+            int x = 0;
+
+            Console.WriteLine("What is the first number?");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 999)
+            {
+                nums[x] = num;
+                x++;
+                Console.WriteLine("What is the next number?");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        private static void outIntegers(out int high, out int low, out int sum, out int avg)
+        {
+
+        }
+        
+    }
+}
---
+++ Reverse3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstInt = 1;
+            int middleInt = 2;
+            int lastInt = 3;
+            Console.WriteLine(firstInt);
+            Console.WriteLine(middleInt);
+            Console.WriteLine(lastInt);
+            Console.WriteLine("vvvvvvvv Reversed Position vvvvvvvv");
+            Reverse(ref firstInt, ref middleInt, ref lastInt);
+            Console.WriteLine(firstInt);
+            Console.WriteLine(middleInt);
+            Console.WriteLine(lastInt);
+        }
+        private static void Reverse(ref int firstInt, ref int middleInt, ref int lastInt)
+        {
+            int holder = firstInt;
+            firstInt = lastInt;
+            lastInt = holder;
+        }
+    }
+}
