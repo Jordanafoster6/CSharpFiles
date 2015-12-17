bf7a95015523428bacaf423c1e52887410283c73
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:33:10 -0600

    OddNums

---
+++ OddNums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 100;
+            for (int i = 1; i <= num1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                   // even number
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
