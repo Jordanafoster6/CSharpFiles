4c9fc9b41ddf77e346ae4a95fd3291ea0759ed83
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:39:25 -0600

    PaintingMethod

---
+++ PaintingMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of your room in inches?");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("What is the width of your room in inches?");
+            double width = double.Parse(Console.ReadLine());
+            double cost = PaintCalculation(length, width);
+            Console.WriteLine(cost.ToString("C"));
+        }
+        private static double PaintCalculation(double length, double width)
+        {
+            double squareFootage = (length * 9 *2) + (width * 9 * 2);
+            double cost = squareFootage * 6;
+            return cost;
+        }
+    }
+}
