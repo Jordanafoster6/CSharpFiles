9947a148b59647b0ae30c365206f5f7079f3397d
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:49:41 -0600

    OrderDemo

---
+++ OrderDemo/Order.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDemo
+{
+    class Order
+    {
+        //fields
+        private string orderNumber;
+        private string cusName;
+        private double quantity;
+        private double totalPrice;
+
+        //properties
+        public string OrderNumber
+        {
+            get
+            {
+                return orderNumber;
+            }
+            set
+            {
+                orderNumber = value;
+            }
+        }
+        public string CusName
+        {
+            get
+            {
+                return cusName;
+            }
+            set
+            {
+                cusName = value;
+            }
+        }
+        public double Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+        public double TotalPrice
+        {
+            get
+            {
+                totalPrice = quantity * 19.95;
+                return totalPrice;
+            }
+        }
+        
+        //constructors
+        public Order()
+        {
+
+        }
+        public Order(string num, string name, double q)
+        {
+            orderNumber = num;
+            cusName = name;
+            quantity = q;
+        }
+
+        //methods
+        public override bool Equals(Object e)  //equals()
+        {
+            bool isEqual;
+            Order temp = (Order)e;
+            if (orderNumber == temp.orderNumber)
+                isEqual = true;
+            else
+                isEqual = false;
+            return isEqual; 
+        }
+
+
+    }
+}
