c87e3ab23ed07aa4cd1df299a1d36e13552acf91
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:37:21 -0600

    ConvertQuartsToLiters

---
+++ ConvertQuartsToLiters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertQuartsToLiters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of quarts and we'll calculate ut into liters.");
+            double quarts = double.Parse(Console.ReadLine());
+            double liters = PaintCalculation(quarts);
+            Console.WriteLine(liters + "liters.");
+            
+        }
+        private static double PaintCalculation(double quarts)
+        {
+            double liter = .966353;
+            double liters = quarts * liter;
+            return liters;
+        }
+     }
+}
