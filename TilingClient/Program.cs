c8af96b0748ad0919b2d8aa405a5444e5c23e05d
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:48:52 -0600

    TilingClient

---
+++ TilingClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room[] Array1 = new Room[10];
+
+            Array1[0] = new Room(122, 1);
+            Array1[1] = new Room(43, 91);
+            Array1[2] = new Room(2, 16);
+            Array1[3] = new Room(46, 54);
+            Array1[4] = new Room(56, 5);
+            Array1[5] = new Room(72, 31);
+            Array1[6] = new Room(637, 74);
+            Array1[7] = new Room(122, 57);
+            Array1[8] = new Room(87, 19);
+            Array1[9] = new Room(183, 61);
+
+
+            for (int i = 0; i < Array1.Length; i++)
+            {
+            Console.WriteLine("Room " + (i + 1));
+            Console.WriteLine("Width: " + Array1[i].Width);
+            Console.WriteLine("Length: " + Array1[i].Length);
+            Console.WriteLine("Square Feet: " + Array1[i].FloorArea);
+            Console.WriteLine("Box Number: " + Array1[i].BoxNum);
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("                                           ");
+            }
+        }
+
+    }
+}
---
+++ TilingClient/Tiling1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilingClient
+{
+    class Room
+    {
+        // Instance Fields
+        private double length;
+        private double width;
+        private double floorArea;
+        private int boxNum;
+
+        public Room()
+        {
+
+        }
+        public Room(double l, double w)
+        {
+            this.length = l;
+            this.width = w;
+            this.floorArea = getFloorArea(this.length, this.width);
+            this.boxNum = getBoxNum(this.floorArea);
+        }
+        private double getFloorArea(double l, double w)
+        {
+            return l * w;
+        }
+        private int getBoxNum(double area)
+        {
+            int x = 1;
+            x += Convert.ToInt32(area / 12);
+            if (x % 12 > 0)
+            {
+                ++x;
+            }
+            return x;
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+
+        }
+        public double FloorArea
+        {
+            get
+            {
+                return floorArea;
+            }
+
+        }
+        public int BoxNum
+        {
+            get
+            {
+                return boxNum;
+            }
+
+        }
+    }
+}
