580b99c9311731d59e1f50c9186ae1cbddf82979
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:46:44 -0600

    ShirtDemo

---
+++ ShirtDemo/Shirt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtDemo
+{
+    class Shirt
+    {
+        //Instances
+        public string material { get; set; }
+        public string color { get; set; }
+        public string size { get; set; }
+
+        public Shirt()
+        {
+
+        }
+        public Shirt(string m, string c, string s)
+        {
+            this.material = m;
+            this.color = c;
+            this.size = s;
+        }
+
+    }
+}
---
+++ ShirtDemo/ShirtClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirtDemo
+{
+    class ShirtClient
+    {
+        static void Main(string[] args)
+        {
+            Shirt[] array1 = new Shirt[5];
+            array1[0] = new Shirt("Cotton", "black", "Large");
+            array1[1] = new Shirt("Nylon", "White", "Small" );
+            array1[2] = new Shirt("Cotton", "yellow", "medium");
+            array1[3] = new Shirt("Nylon", "green", "Large" );
+            array1[4] = new Shirt("Cotton", "black", "Medium" );
+
+            DisplayStrings(array1);
+        }
+
+        // params array method
+        public static void DisplayStrings(params Shirt[] array)
+        {
+            foreach (Shirt x in array)
+            {
+                Console.WriteLine("Material is:" + x.material);
+                Console.WriteLine("Size is:" + x.size);
+                Console.WriteLine("Color is:" + x.color);
+                Console.WriteLine("---------------------------------------");
+            }
+        }
+    }
+}
