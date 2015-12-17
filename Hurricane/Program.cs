c51bb5bbf09acdc40f1d797c2fcbee93c2174ed5
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 08:34:41 -0600

    Hurricane

---
+++ Hurricane/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurricane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the windspeed of the tropical storm? (# of MPH)");
+            double speed = double.Parse(Console.ReadLine());
+            if (speed >= 157)
+            {
+                Console.WriteLine("Category 5 Hurricane.");
+            }
+            else if ((speed < 157) && (speed >= 130))
+            {
+                Console.WriteLine("Category 4 Hurricane");
+            }
+            else if ((speed < 130) && (speed >= 111))
+            {
+                Console.WriteLine("Category 3 Hurricane");
+            }
+            else if ((speed < 111) && (speed >= 96))
+            {
+                Console.WriteLine("Category 2 Hurricane");
+            }
+            else if ((speed < 96) && (speed >= 74))
+            {
+                Console.WriteLine("Category 1 Hurricane");
+            }
+            else
+            {
+                Console.WriteLine("This storm is not a Hurricane.");
+            }
+        }
+    }
+}
