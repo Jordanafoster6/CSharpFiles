ab12c40e7b1adeaadf8e9de182e962abd21be600
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:44:17 -0600

    ConferencesDemo

---
+++ ConferencesDemo/Conferences.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferencesDemo
+{
+    class Conferences : IComparable
+    {
+        private string name;
+        private string date;
+        private int atendeeNum;
+
+        //Constructor
+        public Conferences(string n, string d, int a)
+        {
+            name = n;
+            date = d;
+            atendeeNum = a;
+        }
+
+        // Proerties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+
+        public int AtendeeNum
+        {
+            get
+            {
+                return atendeeNum;
+            }
+            set
+            {
+                atendeeNum = value;
+            }
+        }
+
+        //IComparable
+        int IComparable.CompareTo(object o)
+        {
+            int returnVal;
+            Conferences temp = (Conferences)o;
+            if (this.atendeeNum > temp.atendeeNum)
+            {
+                returnVal = 1;
+            }
+            else if (this.atendeeNum < temp.atendeeNum)
+            {
+                returnVal = -1;
+            }
+            else
+            {
+                returnVal = 0;
+            }
+            return returnVal;
+        }
+    }
+}
---
+++ ConferencesDemo/ConferencesDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConferencesDemo
+{
+    class ConferencesDemo
+    {
+        static void Main(string[] args)
+        {
+            Conferences[] array1 = new Conferences[5];
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                string group;
+                string date;
+                int num;
+                Console.WriteLine("Please enter in your group name.");
+                group = Console.ReadLine();
+                Console.WriteLine("Please enter in the date.");
+                date = Console.ReadLine();
+                Console.WriteLine("Please enter in your attendance number.");
+                num = int.Parse(Console.ReadLine());
+                array1[i] = new Conferences(group, date, num);
+            }
+
+            Array.Sort(array1);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Name: " + array1[i].Name);
+                Console.WriteLine("Date: " + array1[i].Date);
+                Console.WriteLine("Attendee Number: " + array1[i].AtendeeNum);
+                Console.WriteLine("------------------------");
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
