5db30826f1a4120ce3f13558a1f43a63a8f99b2b
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:48:34 -0600

    TaxPayerDemo2

---
+++ TaxPayerDemo2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxPayer2[] array1 = new TaxPayer2[2];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = new TaxPayer2();
+                Console.WriteLine("Please enter in your gross income.");
+                array1[i].GrossIncome = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter in your Social Security Number (SSN)");
+                array1[i].SocialSecurityNumber = Console.ReadLine();
+
+                Console.WriteLine(array1[i]);
+                Console.WriteLine("Your Social Secruity Number is: " + array1[i].SocialSecurityNumber);
+                Console.WriteLine("Your Gross Income is: " + array1[i].GrossIncome.ToString("c"));
+                Console.WriteLine("Your taxes owed for this year are: " + array1[i].TaxOwed.ToString("C"));
+                Console.WriteLine("----------------------------------------------");
+            }
+
+            Array.Sort(array1);
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array1[i]);
+                Console.WriteLine("Your Social Secruity Number is: " + array1[i].SocialSecurityNumber);
+                Console.WriteLine("Your Gross Income is: " + array1[i].GrossIncome.ToString("c"));
+                Console.WriteLine("Your taxes owed for this year are: " + array1[i].TaxOwed.ToString("C"));
+                Console.WriteLine("----------------------------------------------");
+            }
+        }
+    }
+}
---
+++ TaxPayerDemo2/TaxPayer2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo2
+{
+    class TaxPayer2: IComparable
+    {
+        // Instance Fields
+        private string socialSecurityNumber;
+        private double grossIncome;
+        private double taxOwed;
+
+        // Properties
+        public double GrossIncome
+        {
+            get
+            {
+                return grossIncome;
+            }
+            set
+            {
+                grossIncome = value;
+                if (grossIncome < 30000)
+                {
+                    taxOwed = grossIncome * .15;
+                }
+                else
+                {
+                    taxOwed = grossIncome * .28;
+                }
+            }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get
+            {
+                return socialSecurityNumber;
+            }
+            set
+            {
+                socialSecurityNumber = value;
+            }
+        }
+
+        public double TaxOwed
+        {
+            get
+            {
+                return taxOwed;
+            }
+        }
+
+        int IComparable.CompareTo(object o)
+        {
+            int returnVal;
+            TaxPayer2 temp = (TaxPayer2)o;
+            if (this.taxOwed > temp.taxOwed)
+            {
+                returnVal = 1;
+            }
+            else if (this.taxOwed < temp.taxOwed)
+            {
+                returnVal = -1;
+            }
+            else
+            {
+                returnVal = 0;
+            }
+            return returnVal;
+        }
+    }
+}
