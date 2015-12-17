af1979a2220e446e0755d3cd0d5ccf8e250a2929
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:46:03 -0600

    SalesTransactionDemoClient

---
+++ SalesTransactionDemoClient/SalesTransaction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTransactionDemoClient
+{
+    class SalesTransaction
+    {
+        private string salesPerson;
+        private double saleAmount;
+        private double commission;
+        private double commissionRate;
+
+        public SalesTransaction(string name, double amount, double rate)
+        {
+            this.salesPerson = name;
+            this.saleAmount = amount;
+            this.commissionRate = rate;
+
+            this.commission = (saleAmount * commissionRate);
+        }
+
+        public SalesTransaction(string name, double amount)
+        {
+            this.salesPerson = name;
+            this.saleAmount = amount;
+            this.commissionRate = 0;
+        }
+
+        public SalesTransaction(string name)
+        {
+            this.salesPerson = name;
+            this.saleAmount = 0;
+            this.commissionRate = 0;
+            this.commission = 0;
+        }
+
+        public static SalesTransaction  operator+(SalesTransaction first, SalesTransaction second)
+        {
+            double newValue;
+            newValue = first.saleAmount + second.saleAmount;
+
+            string newName;
+            newName = first.salesPerson + "and" + second.salesPerson;
+
+            return (new SalesTransaction(newName, newValue)); 
+        }
+
+        public double CommissionRate
+        {
+            get
+            {
+                return commissionRate;
+            }
+        }
+        public string SalesPerson
+        {
+            get
+            {
+                return salesPerson;
+            }
+            set
+            {
+                salesPerson = value;
+            }
+        }
+        public double SaleAmount
+        {
+            get
+            {
+                return saleAmount;
+            }
+            set
+            {
+                saleAmount = value;
+            }
+        }
+        public double Commission
+        {
+            get
+            {
+                return commission;
+            }
+            set
+            {
+                commission = value;
+            }
+        }
+
+    }
+}
---
+++ SalesTransactionDemoClient/SalesTransactionClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTransactionDemoClient
+{
+    class SalesTransactionClient
+    {
+        static void Main(string[] args)
+        {
+            SalesTransaction trans1 = new SalesTransaction("Jordan", 20.34, .10);
+            SalesTransaction trans2 = new SalesTransaction("andy", 30, .02);
+            SalesTransaction trans3 = new SalesTransaction("alex");
+
+            
+            Console.WriteLine("Transaction 1");
+            Console.WriteLine("Sales Person :" + trans1.SalesPerson );
+            Console.WriteLine("Sale Amount :" + trans1.SaleAmount);
+            Console.WriteLine("Commission Rate :" + trans1.CommissionRate);
+            Console.WriteLine("Commission :" + trans1.Commission);
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.WriteLine("Transaction 2");
+            Console.WriteLine("Sales Person :" + trans2.SalesPerson);
+            Console.WriteLine("Sale Amount :" + trans2.SaleAmount);
+            Console.WriteLine("Commission Rate :" + trans2.CommissionRate);
+            Console.WriteLine("Commission :" + trans2.Commission);
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.WriteLine("Transaction 3");
+            Console.WriteLine("Sales Person :" + trans3.SalesPerson);
+            Console.WriteLine("Sale Amount :" + trans3.SaleAmount);
+            Console.WriteLine("Commission Rate :" + trans3.CommissionRate);
+            Console.WriteLine("Commission :" + trans3.Commission);
+            Console.WriteLine("-----------------------------------------------------------------");
+
+        }
+    }
+}
