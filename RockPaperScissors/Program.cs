7eed69fc3c9108c7d24adeb8a0a065ce595813fc
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 08:35:29 -0600

    RockPaperScissors

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Please enter in rock, paper, or scissors.");
+            input = Console.ReadLine();
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(0, 3);
+            Console.WriteLine(randomNumber);
+
+            if (input == "r" && randomNumber == 0)
+            {
+                
+            }
+
+        }
+    }
+}
