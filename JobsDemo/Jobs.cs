d5761dc9b0acebc165976c04b82a62661500c136
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:44:34 -0600

    JobsDemo

---
+++ JobsDemo/Jobs.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsDemo
+{
+    class Jobs
+    {
+        private string description;
+        private double timeInHours;
+        private double hourRate;
+        private double totalFee;
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public double TimeInHours
+        {
+            get
+            {
+                return timeInHours;
+            }
+            set
+            {
+                timeInHours = value;
+                totalFee = timeInHours * hourRate;
+            }
+        }
+        public double HourRate
+        {
+            get
+            {
+                return hourRate;
+            }
+            set
+            {
+                hourRate = value;
+                totalFee = hourRate * timeInHours;
+            }
+        }
+
+        public Jobs()
+        {
+
+        }
+
+        public Jobs(double time, double hour, string enterJob) // always public capital then params
+        {
+            this.totalFee = time * hour;
+        }
+
+        public static Jobs operator +(Jobs x, Jobs y)
+        {
+            string s = x.description + "and" + y.description;
+            double h = x.timeInHours + y.timeInHours;
+            double r = x.hourRate + y.hourRate;
+            return new Jobs(h, r, s);
+        }
+    }
+}
---
+++ JobsDemo/JobsDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsDemo
+{
+    class JobsDemo
+    {
+        static void Main(string[] args)
+        {
+            Jobs newJob1 = new Jobs(2, 100, "car wash");
+            Console.WriteLine(newJob1.Description);
+            Console.WriteLine(newJob1.TimeInHours);
+            Console.WriteLine(newJob1.HourRate);
+            
+            Jobs newJob2 = new Jobs(4, 200, "carpet cleaning");
+            Console.WriteLine(newJob2.Description);
+            Console.WriteLine(newJob2.TimeInHours);
+            Console.WriteLine(newJob2.HourRate);
+
+            Jobs newJob3 = new Jobs();
+            newJob3 = newJob1 + newJob2;
+            Console.WriteLine(newJob3.Description);
+            Console.WriteLine(newJob3.TimeInHours);
+            Console.WriteLine(newJob3.HourRate);
+            Console.WriteLine(newJob3.);
+        }
+    }
+}
