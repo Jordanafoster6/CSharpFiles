9e758c1739f955ddd868c2bf2a216cafc23ae619
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:37:42 -0600

    Desks

---
+++ Desks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        private static double DeskInput()
+        {
+            Console.WriteLine("Please enter the number of drawers you would like on your desk.");
+            double drawerNum = double.Parse(Console.ReadLine());
+            return drawerNum;
+        }
+        private static string woodType()
+        {
+            Console.WriteLine("Please enter the type of wood you would like.");
+            string wood = Console.ReadLine();
+        }
+        private static string deskCalc()
+        {
+
+        }
+    }
+}
