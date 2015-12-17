3849035702a2a4eb88273a58a750a38f732699eb
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:43:55 -0600

    ClassifiedAd

---
+++ ClassifiedAd/ClassifiedAd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassifiedAd
+{
+    // Class Header (Object)
+    class ClassifiedAd
+    {
+        // Instance Fields
+        private string category;
+        private int wordNum;
+        private double price;
+
+        //Properties 
+        public int WordNum
+        {
+            get { return wordNum; }
+            set
+            {
+                wordNum = value;
+                price = value * .09;
+            }
+        }
+
+        public double Price
+        {
+            get { return price; }
+        }
+
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        //mutator for category
+        public void SetCategory(string category)
+        {
+            this.category = category;
+        }
+
+        //mutator for wordNum
+        public void SetWordNum(int wordNum)
+        {
+            this.wordNum = wordNum;
+        }
+
+    }
+}
---
+++ ClassifiedAd/ClassifiedAdClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassifiedAd
+{
+    class ClassifiedAdClient
+    {
+        static void Main(string[] args)
+        {
+            ClassifiedAd myAd = new ClassifiedAd();
+            myAd.Category = "used cars";
+            myAd.WordNum = 10;
+            Console.WriteLine("The category you chose was {0}. Your ad contains {1} words with a total cost of ${2}",
+                myAd.Category,myAd.WordNum,myAd.Price.ToString("n2"));
+        }
+      
+            
+    }
+}
