855c5749946f734bfad69f90fb93cb5d70894ba5
Jordanafoster6 <Jordanafoster6@gmail.com>
2015-12-17 09:47:23 -0600

    TaxPayerDemo

---
+++ TaxPayerDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaxPayer[] array1 = new TaxPayer[10];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = new TaxPayer();
+                Console.WriteLine("Please enter in your gross income.");
+                array1[i].GrossIncome = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter in your Social Security Number (SSN)");
+                array1[i].SocialSecurityNumber = Console.ReadLine();
+
+                Console.WriteLine(array1[i]);
+                Console.WriteLine("Your Social Secruity Number is: " + array1[i].SocialSecurityNumber);
+                Console.WriteLine("Your Gross Income is: " + array1[i].GrossIncome.ToString("c"));
+                Console.WriteLine("Your taxes owed for this year are: " + array1[i].TaxOwed.ToString("C"));
+                Console.WriteLine("----------------------------------------------");
+            }
+
+        }
+    }
+}
---
+++ TaxPayerDemo/TaxPayer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxPayerDemo
+{
+    class TaxPayer
+    {
+        // Instance Fields
+        private string socialSecurityNumber;
+        private double grossIncome;
+        private double taxOwed;
+
+        // Properties
+        public double GrossIncome
+        {
+            get
+            {
+                return grossIncome;
+            }
+            set
+            {
+                grossIncome = value;
+                if (grossIncome < 30000 )
+                {
+                    taxOwed = grossIncome * .15;
+                }
+                else 
+                {
+                    taxOwed = grossIncome * .28;
+                }
+            }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get
+            {
+                return socialSecurityNumber;
+            }
+            set
+            {
+                socialSecurityNumber = value;
+            }
+        }
+
+        public double TaxOwed
+        {
+            get
+            {
+                return taxOwed;
+            }
+        }
+    }
+}
